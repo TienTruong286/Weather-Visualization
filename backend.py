098a7bdde92203a9752c0fefaacbb691b956abb9
TienTruong286 <86639547+TienTruong286@users.noreply.github.com>
2023-03-12 14:07:25 -0700

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def get_data(place, forecast_days = None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid=f59c610559fce0c3c4b4da08fad00ed2"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8* forecast_days
+    filtered_data = filtered_data[:nr_values]
+
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place = "Tokyo", forecast_days= 3, kind ="Temperature"))
+
